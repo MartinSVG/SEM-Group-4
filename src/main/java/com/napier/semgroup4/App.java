5d59d4d33d4f0dc51fd790f8148d76ae1435d2e0
Alan Gooding <alangooding90@gmail.com>
2023-02-22 21:48:25 -0400

    Changes to output format

--- src/main/java/com/napier/semgroup4/App.java
+++ src/main/java/com/napier/semgroup4/App.java
@@ -19,27 +19,27 @@ public class App
         System.out.println();
 
         // Print all countries in a continent from the largest population to smallest
-        System.out.println("All countries in a continent - Largest Population to Smallest:");
+        System.out.println("All countries in a continent (South America) - Largest Population to Smallest:");
         a.printCountries(a.getCountries("WHERE Continent = 'South America' ",-1));
         System.out.println();
 
         //Print all countries in a region from the largest population to smallest
-        System.out.println("All countries in a region - Largest Population to Smallest:");
+        System.out.println("All countries in a region (Eastern Asia) - Largest Population to Smallest:");
         a.printCountries(a.getCountries("WHERE Region = 'Eastern Asia' ",-1));
         System.out.println();
 
         //Print top N countries in the world from the largest population to smallest
-        System.out.println("All top N countries in the world - Largest Population to Smallest:");
+        System.out.println("All top 5 countries in the world - Largest Population to Smallest:");
         a.printCountries(a.getCountries("",5));
         System.out.println();
 
         //Print top N countries a continent from the largest population to smallest
-        System.out.println("All top N countries in a continent - Largest Population to Smallest:");
+        System.out.println("All top 5 countries in a continent (South America) - Largest Population to Smallest:");
         a.printCountries(a.getCountries("WHERE Continent = 'South America' ",5));
         System.out.println();
 
         //Print top N countries a continent from the largest population to smallest
-        System.out.println("All top N countries in a region - Largest Population to Smallest:");
+        System.out.println("All top 5 countries in a region (Eastern Asia) - Largest Population to Smallest:");
         a.printCountries(a.getCountries("WHERE Region = 'Eastern Asia' ",5));
         System.out.println();
         // Disconnect from database
