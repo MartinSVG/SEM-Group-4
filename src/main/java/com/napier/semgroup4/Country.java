90bdedab1051361e05628bb190c00667dbc500aa
Elizabeth Khosa <lizzie.khosa@gmail.com>
2023-02-23 20:16:06 -0500

    Comments to country class

--- src/main/java/com/napier/semgroup4/Country.java
+++ src/main/java/com/napier/semgroup4/Country.java
@@ -1,10 +1,38 @@
 package com.napier.semgroup4;
 
+
+/**
+ * Represents a Country
+ */
 public class Country {
+    /**
+     * Country ID
+     *
+     */
     public String countryID;
+
+    /**
+     *  Country Name
+     */
     public String name;
+
+    /**
+     *  Country Continent
+     */
     public String continent;
+
+    /**
+     *  Country Region
+     */
     public String region;
+
+    /**
+     *  Country Population
+     */
     public int population;
+
+    /**
+     *  Country Capital
+     */
     public String capital;
 }
