e7d99cb94e17f2ba5bfc222ddcb4049f122a6277
EsaiasBurnette <esaiasburnette@gmail.com>
2023-03-09 03:32:30 -0400

    Top N Cities without comments

--- src/main/java/com/napier/semgroup4/N_City_Report.java
+++ src/main/java/com/napier/semgroup4/N_City_Report.java
@@ -16,7 +16,7 @@ public class N_City_Report {
         int num4 = 3;
         int num5 = 6;
 
-        // Extract City information
+
         ArrayList<City> world = a.getNCities(num1);
         ArrayList<City> continent = a.getNCities(num2);
         ArrayList<City> region = a.getNCities(num3);
@@ -24,28 +24,28 @@ public class N_City_Report {
         ArrayList<City> district = a.getNCities(num5);
 
 
-        //Print a list of all cities in the world organized by largest to smallest population
+
         System.out.println();
         System.out.println("The top " + num1 + " populated cities in the world:");
         a.printAllCities(world);
 
 
-        //Print a list of all cities in a continent organized by largest to smallest population
+
         System.out.println();
         System.out.println("The top " + num2 + " populated cities in a continent:");
         a.printCityContinent(continent);
 
-        //Print a list of all cities in a region organized by largest to smallest population
+
         System.out.println();
         System.out.println("The top " + num3 + " populated cities in a region:");
         a.printCityRegion(region);
 
-        //Print a list of all cities in a country organized by largest to smallest population
+
         System.out.println();
         System.out.println("The top " + num4 + " populated cities in a country:");
         a.printCityCountry(country);
 
-        //Print a list of all cities in a district organized by largest to smallest population
+
         System.out.println();
         System.out.println("The top " + num5 + " populated cities in a district:");
         a.printCityDistrict(district);
