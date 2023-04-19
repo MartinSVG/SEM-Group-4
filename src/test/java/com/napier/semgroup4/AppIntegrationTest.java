d7fe04cef66c74a52bcbce5aa81d67daf90fe53d
Alan Gooding <alangooding90@gmail.com>
2023-04-19 10:51:43 -0400

    Added Integration Tests for City Top N feature

--- src/test/java/com/napier/semgroup4/AppIntegrationTest.java
+++ src/test/java/com/napier/semgroup4/AppIntegrationTest.java
@@ -20,6 +20,48 @@ public class AppIntegrationTest
 
     }
 
+    // Integration Tests for Cities Feature
+    @Test
+    void testTopFivePopulatedCitiesInTheWorld()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("",5);
+        assertEquals(cities.size(),5);
+    }
+
+    @Test
+    void testTopFivePopulatedCitiesInAContinent()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Continent = 'North America'",5);
+        assertEquals(cities.size(),5);
+    }
+
+    @Test
+    void testTopFivePopulatedCitiesInARegion()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Region = 'Caribbean'",5);
+        assertEquals(cities.size(),5);
+    }
+
+    @Test
+    void testTopFivePopulatedCitiesInACountry()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Name = 'India'",5);
+        assertEquals(cities.size(),5);
+    }
+
+    @Test
+    void testTopFivePopulatedCitiesInADistrict()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("city.District = 'Punjab'",5);
+        assertEquals(cities.size(),5);
+    }
+
+    // Integration Tests for Countries Feature
     @Test
     void testTopFivePopulatedCountriesInTheWorld()
     {
