61d407a142c941dd664d36a6b03bc067deef1e16
Alan Gooding <alangooding90@gmail.com>
2023-04-22 08:22:35 -0400

    Added integration test for 'all cities' feature

--- src/test/java/com/napier/semgroup4/AppIntegrationTest.java
+++ src/test/java/com/napier/semgroup4/AppIntegrationTest.java
@@ -20,54 +20,91 @@ public class AppIntegrationTest
 
     }
 
-    // Integration Tests for Cities Feature
+    /* Integration Tests for Cities Feature */
+
+    // All cities in the world
+    @Test
+    void testGetAllCitiesInTheWorld(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("",-1);
+        assertNotNull(cities);
+    }
+    // All cities in a continent
+    @Test
+    void testGetAllCitiesInAContinent(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Continent = 'North America'",-1);
+        assertNotNull(cities);
+    }
+    // All cities in a region
+    @Test
+    void testGetAllCitiesInARegion(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Region = 'Caribbean'",-1);
+        assertNotNull(cities);
+    }
+    // All cities in a country
+    @Test
+    void testGetAllCitiesInACountry(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("country.Name = 'India'",-1);
+        assertNotNull(cities);
+    }
+    // All cities in a district
+    @Test
+    void testGetAllCitiesInADistrict(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities = app.getCities("city.District = 'Punjab'",-1);
+        assertNotNull(cities);
+    }
+    // Top 5 cities in the world
     @Test
     void testTopFivePopulatedCitiesInTheWorld()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCities("",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
-
+    // Top 5 cities in a continent
     @Test
     void testTopFivePopulatedCitiesInAContinent()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCities("country.Continent = 'North America'",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
-
+    // Top 5 cities in a region
     @Test
     void testTopFivePopulatedCitiesInARegion()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCities("country.Region = 'Caribbean'",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
-
+    // Top 5 cities in a country
     @Test
     void testTopFivePopulatedCitiesInACountry()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCities("country.Name = 'India'",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
-
+    // Top 5 cities in a district
     @Test
     void testTopFivePopulatedCitiesInADistrict()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCities("city.District = 'Punjab'",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
 
-    // Integration Tests for Countries Feature
+    /* Integration Tests for Countries Feature */
     @Test
     void testTopFivePopulatedCountriesInTheWorld()
     {
         ArrayList<Country> countries = new ArrayList<Country>();
         countries = app.getCountries("",5);
-        assertEquals(countries.size(),5);
+        assertEquals(5, countries.size());
     }
 
     @Test
@@ -75,6 +112,6 @@ public class AppIntegrationTest
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities = app.getCapitalCities("",5);
-        assertEquals(cities.size(),5);
+        assertEquals(5, cities.size());
     }
 }
